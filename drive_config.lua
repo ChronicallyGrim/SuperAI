e0ab1dd435271427ae8325d6417d6ffce6f4759a
ChronicallyGrim <sammillington700@gmail.com>
2026-01-19 12:08:22 -0700

    Update drive_config.lua

--- drive_config.lua
+++ drive_config.lua
@@ -6,15 +6,15 @@ return {
     -- TOP drive (code storage) - can be nil to use computer root
     top = "top",
     
-    -- LEFT drives (RAM A - temp storage during training)
+    -- LEFT drives (RAID A - permanent data storage)
     left = {"drive_30", "drive_29"},
     
-    -- BACK drives (RAM B - temp storage during training)
+    -- BACK drives (RAM A - temp storage during training)
     back = {"drive_45", "drive_43", "drive_44"},
     
-    -- RIGHT drives (RAID A - permanent data storage)
+    -- RIGHT drives (RAID B - permanent data storage)
     right = {"drive_32", "drive_31"},
     
-    -- BOTTOM drives (RAID B - permanent data storage)
+    -- BOTTOM drives (RAM B - temp storage during training)
     bottom = {"drive_41", "drive_42", "drive_40"}
 }
